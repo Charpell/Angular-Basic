8479ba59ea216508938e7337b65c9010a6a244bb
charpell <charpellumeh@gmail.com>
2017-11-22 10:10:04 +0100

    Initial Commit

---
+++ src/app/components/user.components.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import {PostsService} from '../services/post.service'
+
+@Component({
+  moduleId: module.id,
+  selector: 'user',
+  templateUrl: 'user.component.html',
+  providers: [ PostsService ]
+})
+export class UserComponent  { 
+  name: string;
+  email: string;
+  address: address;
+  hobbies: string[];
+  showHobbies: boolean;
+  posts: Post[]
+
+
+  constructor(private postsService: PostsService) {
+    this.name = 'November';
+    this.email = 'john@gmail.com';
+    this.address = {
+      street: '12 Main st',
+      city: 'Boston',
+      state: 'MA'
+    }
+    this.hobbies = ['Music', 'Movies', 'Sports'];
+    this.showHobbies = false;
+    
+
+    this.postsService.getPosts().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  toggleHobbies(){
+    if(this.showHobbies){
+      return this.showHobbies = false;
+    }
+    this.showHobbies = true;
+  }
+
+  addHobby(hobby){
+    this.hobbies.push(hobby)
+  }
+
+  deleteHobby(i){
+    this.hobbies.splice(i, 1)
+  }
+}
+
+interface address {
+  street: string;
+  city: string;
+  state: string;
+}
+
+interface Post {
+  id:number;
+  title: string;
+  body: string;
+}
